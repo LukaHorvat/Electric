a3157ae045e1b25f57969e066448d87ea0e50eda
Luka Horvat <luka.horvat9@gmail.com>
2012-05-22 15:18:43 +0200

    Preparing graphics

--- Electric/Grid.cs
+++ Electric/Grid.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FireflyGL;
+using FireflyGL.Utility;
 
 namespace Electric
 {
 	class Grid : DisplayObject
 	{
-		private Shape display;
+		private const int SIZE = 30;
+		private const int PADDING = 5;
+
+		private Shape background;
+		private List<List<GridPiece>> gridPieces;
 
 		public Grid(int width, int height)
 		{
-			display = new ColoredShape();
-			AddChild(display);
+			gridPieces = new List<List<GridPiece>>(width);
+			for (int i = 0; i < width; ++i)
+			{
+				gridPieces.Add(new List<GridPiece>(height));
+				for (int j = 0; j < height; ++j)
+				{
+					gridPieces[i].Add(null);
+				}
+			}
 
-			var brush = new ColoredRectangle(0, 0, 20, 20, 0.2F, 0.2F, 0.2F, 1);
+			background = new ColoredShape();
+			AddChild(background);
+
+			var brush = new ColoredRectangle(0, 0, SIZE, SIZE, 0.2F, 0.2F, 0.2F, 1);
 			brush.AddOutline(0.7F, 0.7F, 0.7F, 1);
 
 			for (int i = 0; i < width; ++i)
 			{
 				for (int j = 0; j < height; j++)
 				{
-					brush.X = i * 25;
-					brush.Y = j * 25;
-					brush.DrawToShapeGlobal(display);
+					brush.X = i * (SIZE + PADDING);
+					brush.Y = j * (SIZE + PADDING);
+					brush.DrawToShapeGlobal(background);
 				}
 			}
 		}
--- Electric/World.cs
+++ Electric/World.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FireflyGL;
+using FireflyGL.Utility;
 
 namespace Electric
 {
@@ -18,12 +19,18 @@ namespace Electric
 
 		private Grid grid;
 		private Label performanceLabel;
-		private int performanceRefreshCountdown = 0;
+		private Layer gameLayer;
+		private Layer uiLayer;
 
 		public World()
 		{
+			gameLayer = new Layer();
+			uiLayer = new Layer();
+			AddChild(gameLayer);
+			AddChild(uiLayer);
+
 			performanceLabel = new Label("", new System.Drawing.Font("Courier New", 8), System.Drawing.Brushes.White, new Rectangle(0, 0, 300, 300));
-			AddChild(performanceLabel);
+			uiLayer.AddChild(performanceLabel);
 			performanceLabel.X = 500;
 			performanceLabel.IgnoresCamera = true;
 		}
@@ -31,25 +38,21 @@ namespace Electric
 		public void AddGrid(Grid grid)
 		{
 			this.grid = grid;
-			AddChild(grid);
+			gameLayer.AddChild(grid);
 		}
 
 		public override void UpdateSelf()
 		{
 			base.UpdateSelf();
 
-			if (performanceRefreshCountdown == 0)
+			if (Utility.GetCountdown("performanceRefresh") == 0)
 			{
 				performanceLabel.Text =
 					"Update time: " + Firefly.UpdateTime + "\n" +
 					"Render time: " + Firefly.RenderTime + "\n" +
 					"Total time: " + Firefly.TotalTime + "\n" +
 					"Framerate (60 cap): " + (int)(1000 / Firefly.TotalTime);
-				performanceRefreshCountdown = 20;
-			}
-			else
-			{
-				performanceRefreshCountdown--;
+				Utility.StartCountdown("performanceRefresh", 20);
 			}
 		}
 	}
